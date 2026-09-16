9a7b6594851e52f0b8f64d75885498a0a6807ab5
Hunter <realhunterspal@gmail.com>
2026-09-16 14:11:16 -0600

    Added Makefile and hello_world and updated overal README file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #Hunter Elliott's personal git repo for projects
+
 I will be pushing my personal projects to this GitHub and updating this README as I push more to it.
---
+++ test_c/hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    char name[50];
+    printf("Hello, world!\n");
+    printf("What's your name?\n");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
+}
